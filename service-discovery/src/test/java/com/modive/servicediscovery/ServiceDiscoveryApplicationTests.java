b225b834891d627fa44d8821ed53c5a2fb594c64
Jimin Hwang <jiminh1012@gmail.com>
2025-04-23 14:15:08 +0900

    chore(#1): project setting  (#3)
    * chore(#1): gitignore 추가
    
    * feat : config service first setting
    
    * chore(#1): 불필요한 파일 정리
    
    - config-service가 잘못된 형태로 커밋되어 파일 정리합니다.
    
    * feat(#1): config 서버 생성
    
    - config 서버 생성
    - 깃 Resources 레포지토리에서 불러오도록 설정
    
    * feat(#1): service-discovery 서버 생성
    
    - service-discovery 서버 생성
    - Config 서버로부터 설정 파일 불러와서 실행 (dev 프로파일)
    
    * feat(#1): apigateway-service 서버 생성
    
    - apigateway-service 서버 생성
    - Config 서버로부터 설정 받아와서 실행 (dev 프로파일)
    
    * chore(#1): PR 템플릿 생성
    
    * chore(#1): PR 템플릿 수정
    
    - 가독성을 위해 PR 템플릿 소제목을 볼드에서 헤더 스타일로 변경합니다.
    
    ---------
    
    Co-authored-by: doheegu <doheegu3@gmail.com>

---
+++ service-discovery/src/test/java/com/modive/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.modive.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
